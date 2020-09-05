3179cdc2873190d47b4f8a5485b181acc05f4321
vchigateri <vchigateri@uei.com>
2020-09-05 13:05:59 +0530

    2nd commit

--- CourseOfferingApplicationProject/Services/CoOrdinatorService.cs
+++ CourseOfferingApplicationProject/Services/CoOrdinatorService.cs
@@ -15,32 +15,22 @@ namespace CourseOfferingApplicationProject.Services
          {
 
              List<Cours> courses = new List<Cours>();
+             List<CoOrdinatorView> coOrdinatoList = new List<CoOrdinatorView>();
             foreach(var course in courses)
             {
-                CoOrdinatorView coOrdinator = new CoOrdinatorView();
+                CoOrdinatorView coOrdinatoview = new CoOrdinatorView();
                 List<User> members = new List<User>();
-                coOrdinator.CourseName = course.Name;
-                coOrdinator.CourseDescription = course.Description;
+                coOrdinatoview.CourseName = course.Name;
+                coOrdinatoview.CourseDescription = course.Description;
                 var teachers = (from m in members
-                                where m.Role.Role1 == "Teacher" select members);
-                coOrdinator.Teachers = string.Join(",", teachers.ToArray());
+                                where m.Role.Role1 == "Teacher" select m).ToList();
+                var students = (from m in members
+                                where m.Role.Role1 == "Student" select m).ToList();
+                coOrdinatoview.Teachers = string.Join(",", teachers);
+                coOrdinatoview.Students= string.Join(",", students);
+                coOrdinatoList.Add(coOrdinatoview);
             }
-             //var courselist= dbContext.Courses.Select(x=>x).ToList();
-             //var couseUserslist= dbContext.CourseXUsers.Select(x=>x).ToList();
-             //             var users= dbContext.Users.Select(x=>x).ToList();
-
-             //var recs = from c in couseUserslist
-             //           join cl in courselist
-             //               on c.CourseId equals cl.ID
-             //           //join user in users on c.UserId equals user.UserId
-             //           select new CoOrdinatorView
-             //                      {
-             //                          CourseName = cl.Name,
-             //                          CourseDescription = cl.Description,
-
-             //                      };
-             //return recs.ToList();
-
+             return coOrdinatoList;
          }
     }
 }.
